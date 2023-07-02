9de80d41680b33f0d60a8e75bf9eb922413f2736
joao-cavichiolli <joao.cavichiolli@gmail.com>
2023-07-02 17:05:16 -0300

    ddd

--- app/settings.py
+++ app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--^7yc1*f*sxxoc=#+r3%ok^lxdvq0y1zg8fjgv12&tiw@s+grf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
